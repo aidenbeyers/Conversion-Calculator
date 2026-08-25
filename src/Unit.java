977346128014c642c533c9fc142eee370a7b8c37
Aiden Beyers <aidenbeyers@gmail.com>
2026-08-24 22:29:55 -0400

    Create basic Unit class

---
+++ src/Unit.java
@@ -0,0 +1,10 @@
+public class Unit {
+    
+    private final String NAME;
+    private double value;
+    
+    Unit(String name, double value) {
+        this.NAME = name;
+        this.value = value;
+    }
+}
