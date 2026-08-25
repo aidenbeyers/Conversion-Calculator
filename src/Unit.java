0d8a8b0b55150c8c96acb3ffe5a53c45f0ac0abd
Aiden Beyers <aidenbeyers@gmail.com>
2026-08-24 22:48:59 -0400

    Updated Unit class to be abstract.

--- src/Unit.java
+++ src/Unit.java
@@ -1,10 +1,14 @@
-public class Unit {
+abstract class Unit {
     
     private final String NAME;
+    private final String SYMBOL;
     private double value;
     
-    Unit(String name, double value) {
+    Unit(String name, String symbol, double value) {
         this.NAME = name;
+        this.SYMBOL = symbol;
         this.value = value;
     }
+    
+    abstract boolean convertable(Unit unit1, Unit unit2);
 }
