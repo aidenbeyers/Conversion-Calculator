e1dfe6cf53bef43049ca5d1f2ab49803b560a15f
Aiden Beyers <aidenbeyers@gmail.com>
2026-08-24 23:47:32 -0400

    Added WeightUnit class.

---
+++ src/WeightUnit.java
@@ -0,0 +1,11 @@
+abstract class WeightUnit extends Unit {
+
+    WeightUnit(String name, String symbol, double value) {
+        super(name, symbol, value);
+    }
+
+    @Override
+    boolean convertable(Object unit1, Object unit2) {
+        return unit1 instanceof DistanceUnit && unit2 instanceof WeightUnit;
+    }
+}
