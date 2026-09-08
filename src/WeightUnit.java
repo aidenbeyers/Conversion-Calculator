e1142f9effe33963adf760224ecace0dc3f2c51b
Aiden Beyers <aidenbeyers@gmail.com>
2026-09-08 16:30:55 -0400

    Update convertable method

--- src/WeightUnit.java
+++ src/WeightUnit.java
@@ -5,7 +5,7 @@ abstract class WeightUnit extends Unit {
     }
 
     @Override
-    boolean convertable(Object unit1, Object unit2) {
-        return unit1 instanceof WeightUnit && unit2 instanceof WeightUnit;
+    boolean convertable(Object unit2) {
+        return unit2 instanceof WeightUnit;
     }
 }
