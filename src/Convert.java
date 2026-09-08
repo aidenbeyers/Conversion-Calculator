1422f5b14cb2f8dc112c49676fc60757bedded85
Aiden Beyers <aidenbeyers@gmail.com>
2026-09-08 15:49:46 -0400

    Add methods for validating user input in distance case

--- src/Convert.java
+++ src/Convert.java
@@ -20,6 +20,36 @@ public class Convert {
             
             // If distance is selected, the different distance options will be made available.
             if(input.equals("distance")) {
+                System.out.println("Here are your different units:");
+                System.out.println("kilometer (km)");
+                System.out.println("meter (m)");
+                System.out.println("centimeter (cm)");
+
+                System.out.println("Now you will enter what you would like converted:");
+                System.out.println("Please enter the unit being converted:");
+                
+                // Loops until a valid input for Unit 1 is made.
+                unit1_input:
+                while(true) {
+                    String Unit1 = scanner.next();
+                    if(validDistanceUnit(Unit1)) {
+                        break unit1_input;
+                    }
+                    System.out.println("Invalid input. Try Again.");
+                }
+
+                System.out.println("Now please enter it's value:");
+                
+                // Loops until Unit1 can be assigned a proper value;
+                unit1_value_input:
+                while(true) {
+                    String Unit1_Value = scanner.next();
+                    if(isNumeric(Unit1_Value)) {
+                        Double Unit1_Numeric_Value = Double.parseDouble(Unit1_Value);
+                        break unit1_value_input;
+                    }
+                    System.out.println("Invalid input. Try Again.");
+                }
                 
             } else if(input.equals("weight")) {
                 
@@ -36,6 +66,29 @@ public class Convert {
                     }
                 }
             }
+        }
+    }
+    
+    private static boolean validDistanceUnit(String string) {
+        boolean valid = true;
+        switch(string) {
+            case "kilometer", "Kilometer", "km", "meter", "Meter", "m", "centimeter", "Centimeter", "cm":
+                break;
+            default:
+                valid = false;
+        }
+        return valid;
+    }
+
+    private static boolean isNumeric(String str) {
+        if (str == null || str.isEmpty()) {
+            return false;
+        }
+        try {
+            Double.parseDouble(str);
+            return true; // Catches formats like "123", "-45.67", or "3.14"
+        } catch (NumberFormatException e) {
+            return false;
         }
     }
 }
