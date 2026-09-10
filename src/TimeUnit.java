765349bbe3873350e79cce3d416cc6e28c83b135
Aiden Beyers <aidenbeyers@gmail.com>
2026-09-10 09:37:09 -0400

    Add convert method to TimeUnits

--- src/TimeUnit.java
+++ src/TimeUnit.java
@@ -15,4 +15,31 @@ abstract class TimeUnit extends Unit{
     boolean convertible(String s) {
         return units.contains(s);
     }
+
+    @Override
+    double convert(String s) {
+        switch(super.SYMBOL + "-" + s) {
+            // This section covers cases when converting from a kilogram
+            case "s-mins":
+                return super.value / 60.0;
+            case "s-hrs":
+                return super.value / 3600.0;
+
+            //This section covers cases when converting from a gram
+            case "mins-s":
+                return super.value * 60.0;
+            case "mins-hrs":
+                return super.value / 60.0;
+
+            //This section covers cases when converting from a milligram    
+            case "hrs-s":
+                return super.value * 3600.0;
+            case "hrs-mins":
+                return super.value / 60.0;
+
+            //The default assumes the user attempts to convert a unit to itself.    
+            default:
+                return super.value;
+        }
+    }
 }
